183dea526e78e3072dd9b557e9edaf804c6856c6
jaswinder-kaur <59885074+jaswinder-kaur@users.noreply.github.com>
2021-01-31 11:03:37 -0600

    Update admin.py

--- clientms/clients/admin.py
+++ clientms/clients/admin.py
@@ -1,18 +1,13 @@
 from django.contrib import admin
-
 from .models import Client, Comment
 
 class CommentInline(admin.TabularInline):
     model = Comment
 
-
-
 class ClientAdmin(admin.ModelAdmin):
     inlines = [
         CommentInline,
-
-
-    ]
+  ]
 
 
 admin.site.register(Client, ClientAdmin)
