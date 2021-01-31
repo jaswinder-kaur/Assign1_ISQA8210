d412cb6b07fe2861843d66c3238395aee63efeb6
jaswinder-kaur <59885074+jaswinder-kaur@users.noreply.github.com>
2021-01-31 11:02:42 -0600

    Update urls.py

--- clientms/clientms/urls.py
+++ clientms/clientms/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-
-
 ]
